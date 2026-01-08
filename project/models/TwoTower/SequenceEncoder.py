d01cc6ed4cbeb0cd39cfa7d8b9349923ea665e1e
juankim834 <juankim834@gmail.com>
2026-01-07 22:54:04 -0400

    Fix no sequence position embedding in SequenceEncoder

--- project/models/TwoTower/SequenceEncoder.py
+++ project/models/TwoTower/SequenceEncoder.py
@@ -11,23 +11,32 @@ class SequenceEncoder(nn.Module):
 
         # Movie embedding
         self.item_emb = nn.Embedding(item_count, emb_dim, padding_idx=0)
+
+        self.pos_emb = nn.Embedding(20, emb_dim)
+
         # Self-Attention layer
         self.attention = nn.MultiheadAttention(embed_dim=emb_dim, num_heads=4, batch_first=True)
 
     def forward(self, history_seq, genre_sep):
-
+        
         # history movie seq: [batch, 20]
+        batch_size, seq_len = history_seq.shape
+        # --- Embedding Layer ---
         movie_vec = self.item_emb(history_seq)
 
         # history genre sep: [batch, 20, 3]
         genre_vec_raw = self.genre_emb(genre_sep)
+
         # Transform to [batch, 20]
         genre_vec_pooled = torch.sum(genre_vec_raw, dim=2)
 
+        position = torch.arange(seq_len, device=history_seq.device).unsqueeze(0)
+        pos_vec = self.pos_emb(position)
         
-        # [batch, 20, emb_dim]
-        seq_emb = movie_vec + genre_vec_pooled
+        # Combining
+        seq_emb = movie_vec + genre_vec_pooled + pos_vec
 
+        # --- Attention Layer ---
         # Generate Mask
         # key_padding_mask: [batch, 20], True stands for padding location
         mask = (history_seq == 0)
@@ -36,14 +45,18 @@ class SequenceEncoder(nn.Module):
         # attn_output: [batch, 20, emb_dim]
         attn_output, _ = self.attention(seq_emb, seq_emb, seq_emb, key_padding_mask=mask)
 
-        # --- Last Valid Action ---
+        # Residual connection
+        seq_emb = self.layer_norm(seq_emb + attn_output)
+
+        # --- Pooling (Last Valid Item) ---
         valid_lengths = (~mask).long().sum(dim=1)
-        valid_lengths = torch.where(valid_lengths == 0, torch.tensor(1, device=mask.device))
+        valid_lengths = torch.clamp(valid_lengths, min=1)
 
         last_valid_idx = valid_lengths - 1
-        batch_indices = torch.arange(attn_output.size(0), device=attn_output.device)
+        batch_indices = torch.arange(batch_size, device=attn_output.device)
 
         # [batch, emb_dim]
-        final_seq_vec = attn_output[batch_indices, last_valid_idx, :]
+        final_seq_vec = seq_emb[batch_indices, last_valid_idx, :]
+        final_seq_vec = final_seq_vec * (~mask[:, 0].unsqueeze(-1)) # make sure invalid user = 0
 
         return final_seq_vec
