d193bc77c6690f56cd7ef6a881bc167a211abc75
juankim834 <juankim834@gmail.com>
2026-01-12 19:21:09 -0400

    fix: wrong parameter names

--- project/utils/DataLoader.py
+++ project/utils/DataLoader.py
@@ -1,6 +1,5 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
-import numpy as py
 import pandas as pd
 
 class RecommendationDataset(Dataset):
@@ -17,25 +16,26 @@ class RecommendationDataset(Dataset):
     def __len__(self):
         return len(self.data)
     
-    def __getitem__(self, index):
+    def __getitem__(self, index: int):
         row = self.data.iloc[index]
 
         item_inputs = {
             'movie_id': torch.tensor(row["movie_id_enc"], dtype=torch.long),
-            'year': torch.tensor(row['movei_year_enc'], dtype=torch.long),
+            'year': torch.tensor(row['release_year_enc'], dtype=torch.long),
             'genre': torch.tensor(row['genre_ids'], dtype=torch.long)
         }
 
         user_inputs = {
             'user_id': torch.tensor(row['user_id_enc'], dtype=torch.long),
             'gender': torch.tensor(row['gender_enc'], dtype=torch.long),
+            'age': torch.tensor(row['age_enc']),
             'occup': torch.tensor(row['occupation_enc'], dtype=torch.long),
-            'zip': torch.tensor(row['zip_enc'], dtype=torch.long),
-            'year_rate': torch.tensor(row['year_enc'], dtype=torch.long),
-            'rating_month':torch.tensor(row['rating_month'], dtype=torch.long),
-            'rating_hour':torch.tensor(row['rating_hour'], dtype=torch.long),
-            'rating_weekday': torch.tensor(row['rating_weekday'], dtype=torch.long),
-            'rating_activiry_log': torch.tensor(row['user_activity_log'], dtype=torch.long),
+            'zip_code': torch.tensor(row['zip_enc'], dtype=torch.long),
+            'year': torch.tensor(row['year_enc'], dtype=torch.long),
+            'month':torch.tensor(row['rating_month'], dtype=torch.long),
+            'hour':torch.tensor(row['rating_hour'], dtype=torch.long),
+            'weekday': torch.tensor(row['rating_weekday'], dtype=torch.long),
+            'user_activity': torch.tensor(row['user_activity_log'], dtype=torch.float),
             'hist_movie_ids': torch.tensor(row['hist_movie_ids'], dtype=torch.long),
             'hist_genre_ids': torch.tensor(row['hist_genre_ids'], dtype=torch.long)
         }
