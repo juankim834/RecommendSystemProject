e55b633424dddbf8a2239792dcc48522f5a588aa
juankim834 <juankim834@gmail.com>
2026-01-12 19:19:35 -0400

    feat: add NaN checking of embedding vector

--- project/models/TwoTower/TwoTowerModel.py
+++ project/models/TwoTower/TwoTowerModel.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from Tower import MLP_Tower
-from SequenceEncoder import SequenceEncoder
 
 class TwoTowerModel(nn.Module):
     def __init__(self, user_tower, item_tower, temperature=0.1):
@@ -25,12 +23,15 @@ class TwoTowerModel(nn.Module):
 
         return user_emb, item_emb
 
-    def comput_loss(self, user_emb, item_emb):
+    def compute_loss(self, user_emb, item_emb):
         """
         Use In-batch negatives to compute loss
         
         For ith user, ith item is positive sample, the other (Batch - 1) items are negative sample is negative
         """
+
+        if torch.isnan(user_emb).any():
+            raise RuntimeError("Found NaN in User Embedding")
 
         logits = torch.matmul(user_emb, item_emb.transpose(0, 1))
 
