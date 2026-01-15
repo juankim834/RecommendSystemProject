338342df9bfea56df6231396ffe800004d81d2ba
juankim834 <juankim834@gmail.com>
2026-01-15 13:36:31 -0400

    feat: add compatibility with hard negative

--- project/utils/training_utils.py
+++ project/utils/training_utils.py
@@ -1,5 +1,6 @@
 import torch
 from tqdm import tqdm
+import torch.nn.functional as F
 
 def to_device(data, device):
     if isinstance(data, torch.Tensor):
@@ -50,7 +51,7 @@ def validate(model, loader, item_loader, device, k_list=[10, 20],
     model.eval()
     total_loss = 0
     total_recall = {k: 0.0 for k in k_list}
-    num_batches = 0
+    num_samples = 0
     pbar_validating = tqdm(loader, desc="Validating")
 
     with torch.no_grad():
@@ -64,11 +65,29 @@ def validate(model, loader, item_loader, device, k_list=[10, 20],
         all_item_embs = torch.cat(all_item_embs_list, dim=0)
         all_item_ids = torch.cat(all_item_ids_list, dim=0)
 
+        # Optimization: for faster finding hard negative
+        cpu_ids = all_item_ids.detach().cpu().tolist()
+        id_to_index_map = {id_val: idx for idx, id_val in enumerate(cpu_ids)}
+
         for batch_data in pbar_validating:
             batch_data = to_device(batch_data, device)
 
             user_emb, item_emb = model(batch_data)
-            loss = model.compute_loss(user_emb, item_emb)
+
+            hard_neg_emb = None
+            if 'hard_negatives' in batch_data and 'ids' in batch_data['hard_negatives']:
+                hard_neg_ids = batch_data['hard_negatives']['ids']
+                if hard_neg_ids.sum() > 0:
+                    batch_size, num_negs = hard_neg_ids.shape
+                    hard_neg_ids_flat = hard_neg_ids.view(-1).cpu().tolist()
+                    indices = [id_to_index_map.get(mid, 0) for mid in hard_neg_ids_flat]
+                    indices_tensor = torch.tensor(indices, device=device)
+
+                    hard_neg_emb_flat = F.embedding(indices_tensor, all_item_embs)
+                    
+                    hard_neg_emb = hard_neg_emb_flat.view(batch_size, num_negs, -1)
+            
+            loss = model.compute_loss(user_emb, item_emb, hard_neg_emb)
             total_loss += loss.item()
             targets = _tensor_getter(batch_data, item_id_in_tower_batch_dir)
             scores = torch.matmul(user_emb, all_item_embs.t())
@@ -80,10 +99,10 @@ def validate(model, loader, item_loader, device, k_list=[10, 20],
                 hits = (pred_ids == targets.view(-1, 1)).any(dim=1)
                 total_recall[k] += hits.float().sum().item()
             
-            num_batches += len(targets)
+            num_samples += len(targets)
         
         acc_dict = {}
         for k in k_list:
-            acc = total_recall[k] / num_batches
+            acc = total_recall[k] / num_samples
             acc_dict[k] = acc
     return total_loss / len(loader), acc_dict
